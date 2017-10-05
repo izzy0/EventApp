e732f800cb8c144bfac270dfe0b7a89c26253601
Khannaroth <khannaroth.phay@gmail.com>
2017-10-05 18:59:28 -0500

    added java classes to the last four activities

---
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/EventActivity.java
@@ -0,0 +1,19 @@
+package com.event.app.izhar.eventapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by khanp on 10/5/2017.
+ */
+
+public class EventActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.event_activity);
+    }
+}
+
+
