32f8ca4cb8123c8bb60f684c2e00fb93105ed402
Khannaroth <khannaroth.phay@gmail.com>
2017-10-05 19:38:12 -0500

    worked on eventactivity java class file

--- EventApp/app/src/main/java/com/event/app/izhar/eventapp/EventActivity.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/EventActivity.java
@@ -1,7 +1,10 @@
 package com.event.app.izhar.eventapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by khanp on 10/5/2017.
@@ -9,10 +12,36 @@ import android.support.v7.app.AppCompatActivity;
 
 public class EventActivity extends AppCompatActivity{
 
+    public Button add;
+    public Button search;
+
+    public void init(){
+        add = (Button)findViewById(R.id.admin_add_button);
+        search = (Button)findViewById(R.id.search_button);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View e) {
+
+                 Intent event = new Intent(EventActivity.this, DeleteAdminMembersActivity.class);
+            }
+        });
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View s) {
+
+                Intent search = new Intent(EventActivity.this, EventActivity.class);
+
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_activity);
+        init();
     }
 }
 
--- EventApp/app/src/main/java/com/event/app/izhar/eventapp/GalleryDetailsActivity.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/GalleryDetailsActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 
 public class GalleryDetailsActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
