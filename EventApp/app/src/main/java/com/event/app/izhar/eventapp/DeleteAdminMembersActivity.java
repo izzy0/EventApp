d2794eb9e8052ee540c165db3effc638c6c17fd1
izhar <gk1042mq@metrostate.edu>
2017-10-05 19:08:56 -0500

    created new activity classes and updated manifest

---
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/DeleteAdminMembersActivity.java
@@ -0,0 +1,12 @@
+package com.event.app.izhar.eventapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DeleteAdminMembersActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.delete_admin_members_activity);
+    }
+}
