5966888f6b5c5a33ef62b65b78e4657033c3ec33
izhar <gk1042mq@metrostate.edu>
2017-10-11 15:17:08 -0500

    added login and api classes

--- EventApp/app/src/main/java/com/event/app/izhar/eventapp/CreateUserActivity.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/CreateUserActivity.java
@@ -10,3 +10,70 @@ public class CreateUserActivity extends AppCompatActivity {
         setContentView(R.layout.create_user_activity);
     }
 }
+
+/**
+ *
+ * edit code to work
+ *
+ public class CreateActivity extends AppCompatActivity {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+
+
+super.onCreate(savedInstanceState);
+setContentView(R.layout.create_user);
+
+final EditText etfirst = (EditText) findViewById(R.id.createFName);
+final EditText etlast = (EditText) findViewById(R.id.createLName);
+final EditText etuser = (EditText) findViewById(R.id.createUser);
+final EditText etpass = (EditText) findViewById(R.id.createPass);
+final ImageButton btn = (ImageButton) findViewById(R.id.createCBtn);
+
+btn.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+final String fname = etfirst.getText().toString();
+final String lname = etlast.getText().toString();
+final String uname = etuser.getText().toString();
+final String passw = etpass.getText().toString();
+
+Response.Listener<String> responseListener = new Response.Listener<String>() {
+@Override
+public void onResponse(String response) {
+try {
+Log.i("tagconvertstr", "["+response+"]");
+JSONObject jsonObject = new JSONObject(response);
+boolean success = jsonObject.getBoolean("success");
+System.out.println("boolean json"+success);
+
+if(success){
+
+Intent intent = new Intent(CreateActivity.this, MemberPage.class);
+intent.putExtra("username", uname);
+
+CreateActivity.this.startActivity(intent);
+finish();
+
+}else{
+Toast.makeText(getApplicationContext(), "Username already taken ;(",
+Toast.LENGTH_SHORT).show();
+}
+
+} catch (JSONException e) {
+e.printStackTrace();
+}
+}
+};
+
+RegisterRequest request = new RegisterRequest(fname, lname, uname, passw, responseListener);
+System.out.println("parameters "+request.getParams());
+RequestQueue queue = Volley.newRequestQueue(CreateActivity.this);
+queue.add(request);
+
+}
+});
+}
+
+}
+ */
---
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/RegisterRequest.java
@@ -0,0 +1,35 @@
+package com.event.app.izhar.eventapp;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Izhar on 10/9/2017.
+ */
+
+public class RegisterRequest extends StringRequest {
+
+    private static final String REGISTER_REQUEST_URL = "http://izzysprojects.000webhostapp.com/register.php";
+    private Map<String, String> params;
+
+    public RegisterRequest(String first_name, String last_name, String username, String password,
+                           String email, Response.Listener<String> listener) {
+        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+
+        params = new HashMap<>();
+        params.put("first_name", first_name);
+        params.put("last_name", last_name);
+        params.put("username", username);
+        params.put("password", password);
+        params.put("email", email);
+    }
+
+    @Override
+    public Map<String, String> getParams(){
+        return params;
+    }
+}
+
