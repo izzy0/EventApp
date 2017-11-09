27cf7af789b21b0b6271c4a149a9f22aaa806f05
Khannaroth <khannaroth.phay@gmail.com>
2017-11-09 17:25:10 -0600

    updated branch

--- EventApp/app/src/main/java/com/event/app/izhar/eventapp/LoginActivity.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/LoginActivity.java
@@ -3,108 +3,75 @@ package com.event.app.izhar.eventapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
-import org.json.*;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.util.HashMap;
-import java.util.Map;
-
-
-/**
- * A login screen that offers login via user/password.
- */
 public class LoginActivity extends AppCompatActivity {
 
-    String URL = "http://cq7243tk.000webhostapp.com/index.php";
-    //JSONParser jsonParser = new JSONParser();
-    EditText etUsername, etPassword;
+    private EditText etUsername;
+    private EditText etPassword;
+    private Button btnLogin;
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_two);
+        setContentView(R.layout.activity_login);
 
-        // Set up the login form.
-        Button mLoginButton = (Button) findViewById(R.id.btnLogin);
-        Button mRegisterButton = (Button) findViewById(R.id.btnRegister);
+        etUsername = (EditText) findViewById(R.id.etUsername);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        btnRegister = (Button) findViewById(R.id.btnRegister);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
 
-        mLoginButton.setOnClickListener(new OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                new AttemptLogin();
-            }
-        });
+            public void onClick(View v) {
 
-        mRegisterButton.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //newRegister();
-            }
-        });
-    }
+                final String username = etUsername.getText().toString();
+                final String password = etPassword.getText().toString();
 
-    private class AttemptLogin
-    {
-        final EditText mUsername = (EditText) findViewById(R.id.etUsername);
-        final EditText mPassword = (EditText) findViewById(R.id.etPassword);
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
 
-        String Username = mUsername.getText().toString();
-        String Password = mPassword.getText().toString();
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
 
-                @Override
-                public void onResponse(String response) {
-                    try {
-                        JSONObject obj = new JSONObject(response);
+                            if (success) {
+                                String username = jsonResponse.getString("username");
+                                Intent loginIntent = new Intent(LoginActivity.this, EventNavigationDrawer.class);
+                                loginIntent.putExtra("username", username);
+                                LoginActivity.this.startActivity(loginIntent);
 
-                        if (!obj.getBoolean("error")) {
-                            Toast.makeText(getApplicationContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
-                            finish();
-                            startActivity(new Intent(getApplicationContext(), EventFragment.class));
-                        } else
-                        {
-                            Toast.makeText(getApplicationContext(),obj.getString("message"), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), "Success",
+                                        Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Login Failed",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                    } catch (Exception e) {
-                        e.printStackTrace();
                     }
-                }
-            },
-            new Response.ErrorListener(){
-                public void onErrorResponse(VolleyError error){
-                    Toast.makeText(getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }){
-                protected Map<String,String> getParams() throws AuthFailureError{
-                    Map<String, String> params = new HashMap<>();
-                    params.put("username", Username);
-                    params.put("password", Password);
-                    return params;
-                }
-            };
-    }
+                };
 
-            //String password = args[1];
-            //String name = args [0];
-            //ArrayList params = new ArrayList();
-            //params.add(new BasicNameValuePair("username", name));
-            //params.add(new BasicNameValuePair("password", password));
-            //JSONObject json = jsonParser.makeHttpRequest(URL, "POST", params);
-            //return json;
-
-    public void newRegister(){
-        Toast.makeText(getApplicationContext(),"I Don't Do Shit.", Toast.LENGTH_SHORT).show();
+                LoginRequest loginRequest = new LoginRequest(username, password, responseListener);
+                System.out.println("parameters "+ loginRequest.getParams());
+                RequestQueue queue = Volley.newRequestQueue(LoginActivity.this);
+                queue.add(loginRequest);
+            }
+        });
     }
 }
-
