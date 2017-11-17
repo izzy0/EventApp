8193cc1c0d1ea7baf5afa602f0f439da3a727680
Khannaroth <khannaroth.phay@gmail.com>
2017-11-16 20:36:23 -0600

    created a refresh eventlist button

--- EventApp/app/src/main/java/com/event/app/izhar/eventapp/EventFragment.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventapp/EventFragment.java
@@ -1,5 +1,6 @@
 package com.event.app.izhar.eventapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -13,12 +14,16 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.util.Log;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -30,8 +35,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventFragment extends Fragment implements CreateEvent.OnFragmentInteractionListener {
-    ListView eventListView;
-    String link = "http://cq7243tk.000webhostapp.com/event_list.php";
+
+    private static final String TAG = EventFragment.class.getSimpleName();
+
+    private ListView listView;
+    private EventListviewAdapter adapter;
+    private String finalJSOnObject;
+    private static final String url = "http://cq7243tk.000webhostapp.com/event_list.php";
+    private List<Event> eventList = new ArrayList<Event>();
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -42,7 +54,41 @@ public class EventFragment extends Fragment implements CreateEvent.OnFragmentInt
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.activity_event, container, false);
+        listView = (ListView) view.findViewById(R.id.event_listview);
+        adapter = new EventListviewAdapter(getActivity(), eventList);
+        listView.setAdapter(adapter);
+
+        JsonArrayRequest eventRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d(TAG, response.toString());
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        JSONObject object = response.getJSONObject(i);
+                        Event event = new Event();
+                        event.setEventName(object.getString("eventname"));
+                        event.setEventDate(object.getString("date"));
+
+                        eventList.add(event);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d(TAG, "Error: " + error.getMessage());
+            }
+        });
+
+        AppController.getInstance().addToRequestQueue(eventRequest);
+
+
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab);
+        FloatingActionButton refresh = (FloatingActionButton) view.findViewById(R.id.event_refresh);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,9 +99,44 @@ public class EventFragment extends Fragment implements CreateEvent.OnFragmentInt
             }
         });
 
-        eventListView = (ListView) eventListView.findViewById(R.id.event_listview);
-        EventInput input = new EventInput(getContext(), link, eventListView);
-        input.execute();
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                JsonArrayRequest eventRequest = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d(TAG, response.toString());
+
+                        for (int i = 0; i < response.length(); i++) {
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                Event event = new Event();
+                                event.setEventName(object.getString("eventname"));
+                                event.setEventDate(object.getString("date"));
+
+                                eventList.add(event);
+
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.d(TAG, "Error: " + error.getMessage());
+                    }
+                });
+
+                AppController.getInstance().addToRequestQueue(eventRequest);
+
+            }
+        });
+        return view;
+    }
+
+//        listView = (ListView) listView.findViewById(R.id.event_listview);
+
 
 
 
@@ -100,13 +181,78 @@ public class EventFragment extends Fragment implements CreateEvent.OnFragmentInt
 //                }
 //            }
 //        });
-        return view;
-    }
+//        return view;
+//    }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
     }
+
+//    private class EventParse extends AsyncTask<Void, Void, Void> {
+//
+//        public Context context;
+//
+//        // Creating List of Subject class.
+//        List<EventContext> CustomEventNamesList;
+//
+//        public EventParse(Context context) {
+//
+//            this.context = context;
+//        }
+//
+//        @Override
+//        protected void onPreExecute() {
+//
+//            super.onPreExecute();
+//        }
+//
+//        @Override
+//        protected Void doInBackground(Void... arg0) {
+//
+//            try {
+//
+//                if (finalJSOnObject != null) {
+//
+//                    JSONArray jsonArray = null;
+//                    try {
+//                        jsonArray = new JSONArray(finalJSOnObject);
+//                        JSONObject jsonObject;
+//                        EventContext eventContext;
+//                        CustomEventNamesList = new ArrayList<EventContext>();
+//
+//                        for (int i = 0; i < jsonArray.length(); i++) {
+//
+//                            eventContext = new EventContext();
+//                            jsonObject = jsonArray.getJSONObject(i);
+//
+//                            eventContext.event_name = jsonObject.getString("eventname");
+//
+//                            eventContext.event_date = jsonObject.getString("date");
+//
+//                            CustomEventNamesList.add(eventContext);
+//                        }
+//                    } catch (JSONException e) {
+//                        // TODO Auto-generated catch block
+//                        e.printStackTrace();
+//                    }
+//                }
+//            } catch (Exception e) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void result)
+//
+//        {
+//            EventListviewAdapter adapter = new EventListviewAdapter(CustomEventNamesList, context);
+//            listView.setAdapter(adapter);
+//
+//        }
+//    }
 }
 
 //public class EventFragment extends Fragment implements CreateEvent.OnFragmentInteractionListener {
@@ -172,74 +318,6 @@ public class EventFragment extends Fragment implements CreateEvent.OnFragmentInt
 //    }
 
 
-//    private class ParseJSonDataClass extends AsyncTask<Void, Void, Void> {
-//
-//        public Context context;
-//
-//        // Creating List of Subject class.
-//        List<EventContext> CustomEventNamesList;
-//
-//        public ParseJSonDataClass(Context context) {
-//
-//            this.context = context;
-//        }
-//
-//        @Override
-//        protected void onPreExecute() {
-//
-//            super.onPreExecute();
-//        }
-//
-//        @Override
-//        protected Void doInBackground(Void... arg0) {
-//
-//            try {
-//
-//                if (FinalJSonObject != null) {
-//
-//                    JSONArray jsonArray = null;
-//                    try {
-//                        jsonArray = new JSONArray(FinalJSonObject);
-//                        JSONObject jsonObject;
-//                        EventContext eventContext;
-//                        CustomEventNamesList = new ArrayList<>();
-//
-//                        for (int i = 0; i < jsonArray.length(); i++) {
-//
-//                            eventContext = new EventContext();
-//                            jsonObject = jsonArray.getJSONObject(i);
-//
-//                            eventContext.event_name = jsonObject.getString("event name");
-//
-//                            eventContext.event_date = jsonObject.getString("date");
-//
-//                            CustomEventNamesList.add(eventContext);
-//                        }
-//                    } catch (JSONException e) {
-//                        // TODO Auto-generated catch block
-//                        e.printStackTrace();
-//                    }
-//                }
-//            } catch (Exception e) {
-//                // TODO Auto-generated catch block
-//                e.printStackTrace();
-//            }
-//            return null;
-//        }
-//
-//        @Override
-//        protected void onPostExecute(Void result)
-//
-//        {
-//            // After all done loading set complete CustomSubjectNamesList with application context to ListView adapter.
-//            EventListviewAdapter adapter = new EventListviewAdapter(CustomEventNamesList, context);
-//
-//            // Setting up all data into ListView.
-//            listView.setAdapter(adapter);
-//
-//        }
-//    }
-
 //    @Override
 //    public void onFragmentInteraction(Uri uri) {
 //
@@ -248,26 +326,5 @@ public class EventFragment extends Fragment implements CreateEvent.OnFragmentInt
 //}
 
 
-//
-//
-//
-//
-//
-//        return view;
-//    }
-//
-
-//    private class GetHttpResponse extends AsyncTask<void
-//
-//    void void>
-//
-//    {
-//        private Context context;
-//        String result;
-//        List<EventContext> eventList;
-//        public GetHttpResponse(Context context) {
-//        this.context = context;
-//    }
-//    }
 
 
