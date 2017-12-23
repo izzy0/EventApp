06184bed44fc8544e7dc67281d7c688c6529317b
izhar <gk1042mq@metrostate.edu>
2017-12-23 13:09:41 -0600

    Refactor: moved event list adapter to adapter package

--- EventApp/app/src/main/java/com/event/app/izhar/eventappbeta/Adapter/EventListviewAdapter.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventappbeta/Adapter/EventListviewAdapter.java
@@ -1,23 +1,17 @@
-package com.event.app.izhar.eventappbeta;
+package com.event.app.izhar.eventappbeta.Adapter;
 
 
 import android.app.Activity;
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.TextView;
 
-import com.android.volley.toolbox.ImageLoader;
-import com.android.volley.toolbox.NetworkImageView;
+import com.event.app.izhar.eventappbeta.EventContext;
+import com.event.app.izhar.eventappbeta.R;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- EventApp/app/src/main/java/com/event/app/izhar/eventappbeta/EventFragment.java
+++ EventApp/app/src/main/java/com/event/app/izhar/eventappbeta/EventFragment.java
@@ -20,6 +20,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.event.app.izhar.eventappbeta.Adapter.EventListviewAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONException;
